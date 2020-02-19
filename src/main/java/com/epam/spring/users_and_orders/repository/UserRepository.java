d2c62bfcf055c70faf9423e5e1d1a7011de194d9
Vladislav Shikhanov <vladislav_shikhanov@epam.com>
2020-02-19 20:25:15 +0300

    support of java8 add, gitignore update

--- src/main/java/com/epam/spring/users_and_orders/repository/UserRepository.java
+++ src/main/java/com/epam/spring/users_and_orders/repository/UserRepository.java
@@ -5,6 +5,7 @@ import com.epam.spring.users_and_orders.model.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
@@ -14,11 +15,11 @@ public class UserRepository {
 
   private static final List<User> USERS;
 
-  private static final List<String> USERS_NAMES = List.of("Ivan", "Andrey", "Maxim", "Iluya", "Alexey", "John", "Petr",
-      "Semeon", "Artem", "Vladimir", "Donald");
-  private static final List<String> USERS_LAST_NAMES = List.of("Ivanov", "Andreev", "Maximov", "Iluyich", "Alexeev",
-      "Smith", "Petrov", "Semenov", "Artemov", "Putin", "Trump");
-  private static final List<String> ORDERS_NAMES = List.of("Potato", "Tomato", "Cucumber", "Coffee", "Tea",
+  private static final List<String> USERS_NAMES = Arrays.asList("Ivan", "Andrey", "Maxim", "Iluya", "Alexey", "John",
+      "Petr", "Semeon", "Artem", "Vladimir", "Donald");
+  private static final List<String> USERS_LAST_NAMES = Arrays.asList("Ivanov", "Andreev", "Maximov", "Iluyich",
+      "Alexeev", "Smith", "Petrov", "Semenov", "Artemov", "Putin", "Trump");
+  private static final List<String> ORDERS_NAMES = Arrays.asList("Potato", "Tomato", "Cucumber", "Coffee", "Tea",
       "Bread", "Olive Oil", "Bubble gum", "Magazine", "Pork", "Coca-cola");
 
   private final static Random random = new Random();
